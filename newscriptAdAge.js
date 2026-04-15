0b78cc336a45e43822405c54cef8585f9fb75ee4
pperedaticgrup <ppereda@ticgrup.com>
2026-04-15 10:26:31 +0200

    Update newscriptAdAge.js

--- newscriptAdAge.js
+++ newscriptAdAge.js
@@ -3,10 +3,64 @@ document.addEventListener("DOMContentLoaded", function() {
     const signatureContainer = document.getElementById("signature-container");
 
     const officeTexts = {
-        BARCELONA: `LAS MONTAÑAS DEL CANADA S.L., informs that all the data received through online forms and/or by email will be processed with strict confidentiality in accordance with the Privacy and Security Policy of LAS MONTAÑAS DEL CANADA S.L., as well as with Regulation (EU) 2016/679... (resto del texto)`,
-        LONDON: `CANADA LONDON LTD informs that all the data received through online forms and/or by email will be processed with strict confidentiality in accordance with the Privacy and Security Policy of CANADA LONDON LTD as well as with UK-GDPR... (resto del texto)`,
-        "LOS ANGELES": `CANADA LOS ANGELES INC. informs that all the data received through online forms and/or by email will be processed with strict confidentiality in accordance with the Privacy and Security Policy of CANADA LOS ANGELES INC. as well as with the federal/state legal and regulatory framework... (resto del texto)`
+         BARCELONA: `LAS MONTAÑAS DEL CANADA S.L., informs that all the data received through 
+		online forms and/or by email will be processed with strict 
+		confidentiality in accordance with the Privacy and Security Policy of 
+		LAS MONTAÑAS DEL CANADA S.L., as well as with Regulation (EU) 2016/679 
+		of the European Parliament and of the Council of 27 April 2016 on the 
+		protection of natural persons with regard to the processing of personal 
+		data and on the free movement of such data (General Data Protection 
+		Regulation or GDPR). The purpose of processing these data is to respond 
+		to the communications received and, if applicable, maintain the 
+		corresponding contractual relations. The lawful basis being your 
+		consent, performance of pre-contractual or contractual measures, 
+		compliance with legal obligations and/or legitimate interests pursued by
+			LAS MONTAÑAS DEL CANADA S.L. To exercise the rights of access, 
+		suppression/deletion, rectification, opposition, portability or 
+		limitation, and for any clarification, you can contact us by sending us 
+		an email to&nbsp;<a href="mailto:info@canadacanada.com" style="background-color:transparent;color:rgb(17,85,204)" target="_blank">info@canadacanada.com</a>.
+		In any event, the interested have the right to file a claim in front of
+		the appropriate Data Protection Authority. The personal data provided 
+		will be kept for the time legally or contractually foreseen or while 
+		your consent is maintained. The collected data will not be transferred, 
+		with the exception of legal obligation or permission. Please contact&nbsp;<a href="mailto:info@canadacanada.com" style="background-color:transparent;color:rgb(17,85,204)" target="_blank">info@canadacanada.com</a>&nbsp;immediately
+		about any change or modification related to the information you 
+		provided, in order that it responds truthfully to your current personal 
+		situation.`,
+        
+        LONDON: `CANADA LONDON LTD informs that all the data received through online forms and/or
+		 by email will be processed with strict confidentiality in accordance with the Privacy and 
+		 Security Policy of CANADA LONDON LTD as well as with UK-GDPR (United Kingdom General Data 
+		 Protection Regulation). The purpose of processing these data is to respond to the communications 
+		 received and, if applicable, maintain the corresponding contractual relations. The lawful 
+		 basis being your consent, performance of pre-contractual or contractual measures, compliance 
+		 with legal obligations and/or legitimate interests pursued by CANADA LONDON LTD. To exercise 
+		 the rights of access, suppression/deletion, rectification, opposition, portability or limitation, 
+		 and for any clarification, you can contact us by sending an email to&nbsp;<a href="mailto:london@canadacanada.com" style="background-color:transparent;color:rgb(17,85,204)" target="_blank">london@canadacanada.com</a>.
+		 In any event, the interested have the right to file a claim in front of the appropriate Data Protection Authority. 
+		 The personal data provided will be kept for the time legally or contractually foreseen or while 
+		 your consent is maintained. The collected data will not be transferred, with the exception of 
+		 legal obligation or permission. Please contact&nbsp;<a href="mailto:london@canadacanada.com" style="background-color:transparent;color:rgb(17,85,204)" target="_blank">london@canadacanada.com</a>&nbsp;immediately about any 
+		 change or modification related to the information you provided, in order that it responds 
+		 truthfully to your current personal situation.`,
+        
+        "LOS ANGELES": `CANADA LOS ANGELES INC. informs that all the data received through online forms 
+		and/or by email will be processed with strict confidentiality in accordance with the Privacy and 
+		Security Policy of CANADA LOS ANGELES INC. as well as with the federal/state legal and regulatory framework. 
+		The purpose of processing these data is to respond to the communications received and, if applicable, 
+		maintain the corresponding contractual relations. The lawful basis being your consent, performance of 
+		pre-contractual or contractual measures, compliance with legal obligations and/or legitimate interests 
+		pursued by CANADA LOS ANGELES INC. To exercise the rights of access, suppression/deletion, rectification, 
+		opposition, portability or limitation, and for any clarification, you can contact us by sending an email 
+		to&nbsp;<a href="mailto:losangeles@canadacanada.com" style="background-color:transparent;color:rgb(17,85,204)" target="_blank">losangeles@canadacanada.com</a>. 
+		In any event, the interested have the right to file a claim in front of 
+		the appropriate Data Protection Authority. The personal data provided will be kept for the time legally 
+		or contractually foreseen or while your consent is maintained. The collected data will not be transferred, 
+		with the exception of legal obligation or permission. Please contact&nbsp;<a href="mailto:losangeles@canadacanada.com" style="background-color:transparent;color:rgb(17,85,204)" target="_blank">losangeles@canadacanada.com</a>&nbsp;immediately 
+		about any change or modification related to the information you provided, in order that it responds truthfully 
+		to your current personal situation.`
     };
+
 
     form.addEventListener("submit", function(event) {
         event.preventDefault();
