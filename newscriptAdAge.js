5d53b889420d9cfb9ae08d21e022c533bae90dc4
pperedaticgrup <ppereda@ticgrup.com>
2026-04-15 11:32:49 +0200

    Update newscriptAdAge.js

--- newscriptAdAge.js
+++ newscriptAdAge.js
@@ -80,48 +80,45 @@ document.addEventListener("DOMContentLoaded", function() {
         const urlLogoAdAge = "https://lh3.googleusercontent.com/d/1N9QqMSv42j9pYSgS25xJXmjG2qf_aJ2T";
 
         // HTML Blindado para Outlook (Windows/Mac) y Gmail
-        const signatureHTML = `
-            <div style="direction: ltr; font-family: Arial, sans-serif;">
-                <table cellpadding="0" cellspacing="0" border="0" style="width: 480px; border-collapse: collapse; background-color: transparent;">
-                    <tbody>
-                        <tr>
-                            <td style="padding: 10px 0px; font-family: Arial, Helvetica, sans-serif; line-height: 1.2;">
-                                <div style="margin-bottom: 2px;">
-                                    <span style="font-size: 15px; font-weight: bold; color: #1a1a1a; text-transform: uppercase;">${name} ${surname}</span>
-                                    <span style="font-size: 14px; color: #666666; margin-left: 4px;">${genderText}</span>
-                                </div>
-                                <div style="margin-bottom: 12px;">
-                                    <span style="font-size: 13px; color: #1a1a1a; text-transform: uppercase; font-weight: bold;">${position}</span>
-                                </div>
-                                
-                                <div style="margin-bottom: 10px;">
-                                    <img src="https://lh7-rt.googleusercontent.com/docsz/AD_4nXcvGQVV9BEZ8Xk_AqwPTunNSiz_7cD03EGbOoh_y38_BTFXuK85tjbIH0stuEiZuwVgt5eW8mNN8tvVqOyBbq0drlFJMEH5rPOkEOkx_LD49uwuQUIuLqLwu43aMUzfqcya8Wg3SgVDwXvFy-sK6vo7BIuh?key=0yoYn1Wk--m5eohp_i2BZxI4"
-									width="100" height="auto" alt="CANADA" style="display: block; width: 100px; max-width: 100px; border: 0;">
-                                </div>
+        cconst signatureHTML = `
+    <table cellpadding="0" cellspacing="0" border="0" style="background-color: #ffffff; border-collapse: collapse; width: 480px; font-family: 'Roboto Condensed', sans-serif !important;">
+        <tbody>
+            <tr>
+                <td style="padding: 10px 10px 5px; background-color: #ffffff;">
+                    <p style="margin: 0px; font-size: 12px; color: rgb(44,46,53); line-height: 1.2; font-family: 'Roboto Condensed', sans-serif !important;">
+                        ${name} ${surname} ${genderText}
+                    </p>
+                    
+                    <p style="margin: 0px; font-size: 12px; padding-bottom: 10px; color: rgb(44,46,53); line-height: 1.2; font-family: 'Roboto Condensed', sans-serif !important;">
+                        ${position}
+                    </p>
 
-                                <div style="margin-top: 8px;">
-                                    <p style="margin: 0; font-size: 12px; color: #2c2e35;">${phone}</p>
-                                    <p style="margin: 0; font-size: 12px;">
-                                        <a href="https://www.canadacanada.com/" style="color: #2c2e35; text-decoration: none; font-weight: bold;" target="_blank">CANADACANADA.COM</a>
-                                    </p>
-                                </div>
-                                
-                                <div style="margin-top: 15px;">
-                                    <img src="https://lh3.googleusercontent.com/d/1N9QqMSv42j9pYSgS25xJXmjG2qf_aJ2T" width="140" height="auto" alt="Ad Age" style="display: block; width: 140px; max-width: 140px; border: 0;">
-                                </div>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td style="padding-top: 15px; border-top: 1px solid #eeeeee;">
-                                <p style="margin: 0; font-size: 10px; color: #888888; line-height: 1.4; text-align: justify; font-family: Arial, sans-serif;">
-                                    ${officeText}
-                                </p>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        `;
+                    <p style="margin: 0px; font-size: 12px; padding-bottom: 7px; line-height: 1.2; font-family: 'Roboto Condensed', sans-serif !important;">
+                        <img src="https://lh7-rt.googleusercontent.com/docsz/AD_4nXcvGQVV9BEZ8Xk_AqwPTunNSiz_7cD03EGbOoh_y38_BTFXuK85tjbIH0stuEiZuwVgt5eW8mNN8tvVqOyBbq0drlFJMEH5rPOkEOkx_LD49uwuQUIuLqLwu43aMUzfqcya8Wg3SgVDwXvFy-sK6vo7BIuh?key=0yoYn1Wk--m5eohp_i2BZxI4" width="75" height="auto" alt="Logo" style="display: block; width: 75px; max-width: 75px; border: 0;">
+                    </p>
+
+                    <p style="margin: 0px; font-size: 12px; color: rgb(44,46,53); padding-top: 5px; line-height: 1.2; font-family: 'Roboto Condensed', sans-serif !important;">
+                        ${phone}
+                    </p>
+                    <p style="margin: 0px; font-size: 12px; color: rgb(44,46,53); line-height: 1.2; font-family: 'Roboto Condensed', sans-serif !important;">
+                        <a href="https://www.canadacanada.com/" style="background-color: transparent; color: rgb(44,46,53); text-decoration: none; font-family: 'Roboto Condensed', sans-serif !important;" target="_blank">CANADACANADA.COM</a>
+                    </p>
+
+                    <div style="margin-top: 10px;">
+                        <img src="https://lh3.googleusercontent.com/d/1N9QqMSv42j9pYSgS25xJXmjG2qf_aJ2T" width="140" height="auto" alt="Logo Ad Age" style="display: block; width: 140px; max-width: 140px; border: 0;">
+                    </div>
+                </td>
+            </tr>
+            <tr>
+                <td style="padding: 10px; background-color: #ffffff; border-top: 1px solid #eeeeee;">
+                    <p style="margin: 0px; font-size: 0.6em; color: rgb(211,210,212); line-height: 1.5; font-family: 'Roboto Condensed', sans-serif !important; text-align: justify;">
+                        ${officeText}
+                    </p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+`;
 
         signatureContainer.innerHTML = signatureHTML;
         document.getElementById("show-signature").style.display = "block";
